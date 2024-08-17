5cf39fa8acf1c32e8ada7d9c51dda64ca3a5e520
KaLiMali555 <kirtanmali555@gmail.com>
2024-08-17 13:06:46 +0000

    Initial commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  { "scottmckendry/cyberdream.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "cyberdream",
+    },
+  },
+}
