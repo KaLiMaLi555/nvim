bd4e4eed0f5f8a5a961c620f815d533403e019f4
KaLiMali555 <kirtanmali555@gmail.com>
2024-08-21 17:06:11 +0000

    Add: projects keybind

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,35 @@
+return {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    keys = {
+        { "<C-p>", require("telescope.builtin").git_files, { desc = "Git Files" } },
+        {
+            "<leader>pw",
+            function()
+                local word = vim.fn.expand("<cword>")
+                require("telescope.builtin").grep_string({ search = word })
+            end,
+            { desc = "Telescope search current word" },
+        },
+        {
+            "<leader>pW",
+            function()
+                local word = vim.fn.expand("<cWORD>")
+                require("telescope.builtin").grep_string({ search = word })
+            end,
+            { desc = "Telescope search current WORD" },
+        },
+        {
+            "<leader>P",
+            require("telescope").extensions.projects.projects,
+            { desc = "Search Projects" },
+        },
+    },
+
+    config = function()
+        require("telescope").setup({})
+        require("telescope").load_extension("projects")
+    end,
+}
