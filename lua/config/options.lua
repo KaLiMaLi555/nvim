14cb19f77c2bb3b9d19c48564a3d1a83c34bc869
KaLiMali555 <kirtanmali555@gmail.com>
2024-08-21 17:37:32 +0000

    Add: Setting useful options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,9 +2,26 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.opt.termguicolors = true
+
 vim.opt.scrolloff = 15
 vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
 
 vim.g.autoformat = false
+
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+vim.opt.colorcolumn = "90"
