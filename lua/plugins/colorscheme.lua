eabe10eb86f40f9e285099dde726320fbf1a3f64
KaLiMaLi555 <kirtanmali555@gmail.com>
2024-08-31 16:47:38 +0530

    Change colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,29 @@
 return {
     { "scottmckendry/cyberdream.nvim" },
 
+    {
+        "folke/tokyonight.nvim",
+        name = "tokyonight",
+        config = function()
+            require("tokyonight").setup({
+                style = "night",
+                transparent = true,
+                terminal_colors = true,
+                styles = {
+                    sidebars = "dark",
+                    floats = "dark",
+                },
+            })
+            vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+            vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+        end,
+    },
+
     -- Configure LazyVim to load gruvbox
     {
         "LazyVim/LazyVim",
         opts = {
-            colorscheme = "cyberdream",
+            colorscheme = "tokyonight",
         },
     },
 }
