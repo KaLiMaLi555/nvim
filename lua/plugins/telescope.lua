d58cbfcd6022bad1de2e9da01fb0eeb08dc357ea
KaLiMaLi555 <kirtanmali555@gmail.com>
2024-09-01 08:34:11 +0530

    feat: telescope and neotree changes

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,11 @@ return {
     "nvim-telescope/telescope.nvim",
     dependencies = {
         "nvim-lua/plenary.nvim",
+        {
+            "nvim-telescope/telescope-fzf-native.nvim",
+            build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+        },
+        "nvim-telescope/telescope-frecency.nvim",
     },
     keys = {
         { "<C-p>", require("telescope.builtin").git_files, { desc = "Git Files" } },
@@ -26,10 +31,31 @@ return {
             require("telescope").extensions.projects.projects,
             { desc = "Search Projects" },
         },
+        {
+            "<leader>ps",
+            function()
+                require("telescope.builtin").grep_string({
+                    search = vim.fn.input("Grep > "),
+                })
+            end,
+            { desc = "Search any word" },
+        },
     },
 
     config = function()
-        require("telescope").setup({})
+        require("telescope").setup({
+            extensions = {
+                fzf = {
+                    fuzzy = true, -- false will only do exact matching
+                    override_generic_sorter = true, -- override the generic sorter
+                    override_file_sorter = true, -- override the file sorter
+                    case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+                    -- the default case_mode is "smart_case"
+                },
+            },
+        })
         require("telescope").load_extension("projects")
+        require("telescope").load_extension("fzf")
+        require("telescope").load_extension("frecency")
     end,
 }
