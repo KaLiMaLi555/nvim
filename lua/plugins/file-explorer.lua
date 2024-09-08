e4a4800a429cc7df0d9a0e54339d049d9f7ef7b0
KaLiMaLi555 <kirtanmali555@gmail.com>
2024-09-09 00:02:20 +0530

    fix: file explorer bindings

--- lua/plugins/file-explorer.lua
+++ lua/plugins/file-explorer.lua
@@ -12,18 +12,56 @@ return {
             {
                 "<leader>e",
                 function()
-                    vim.cmd("silent! Neotree position=float toggle=true dir=%:p:h reveal_file=%:p <CR>")
+                    vim.cmd(
+                        "silent! Neotree position=float toggle=true dir=%:p:h reveal_file=%:p <CR>"
+                    )
                 end,
                 desc = "Open NeoTree File Explorer",
             },
             { "<leader>E", false },
         },
+        config = function()
+            vim.notify("NeoTree Loaded", vim.log.levels.INFO, { title = "NeoTree" })
+            require("neo-tree").setup({
+                filesystem = {
+                    follow_current_file = {
+                        enabled = true,
+                    },
+                    window = {
+                        position = "float",
+                    },
+                    hijack_netrw_behavior = "disabled",
+                },
+            })
+        end,
     },
     {
         "stevearc/oil.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
         config = function()
-            require("oil").setup()
+            require("oil").setup({
+                skip_confirm_for_simple_edits = true,
+                keymaps = {
+                    ["/"] = "actions.toggle_hidden",
+                    ["<C-s>"] = false,
+                    ["s"] = {
+                        "actions.select",
+                        opts = { vertical = true },
+                        desc = "Open the entry in a vertical split",
+                    },
+                    ["h"] = {
+                        "actions.select",
+                        opts = { horizontal = true },
+                        desc = "Open the entry in a horizontal split",
+                    },
+                    ["t"] = {
+                        "actions.select",
+                        opts = { tab = true },
+                        desc = "Open the entry in new tab",
+                    },
+                    ["q"] = "actions.close",
+                },
+            })
         end,
         keys = {
             { "<leader>E", "<Cmd>Oil --float<CR>", desc = "Open Oil File Explorer" },
