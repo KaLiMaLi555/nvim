30875e03d7147cdbc6867b1b5aeb47aed761753d
KaLiMaLi555 <kirtanmali555@gmail.com>
2024-09-20 09:37:25 +0530

    Better undotree

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,7 +1,12 @@
 return {
-    "mbbill/undotree",
-
-    config = function()
-        vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
-    end,
+    "jiaoshijie/undotree",
+    dependencies = "nvim-lua/plenary.nvim",
+    config = true,
+    keys = {
+        {
+            "<localleader>u",
+            "<cmd>lua require('undotree').toggle()<cr>",
+            desc = "Toggle Undo Tree",
+        },
+    },
 }
