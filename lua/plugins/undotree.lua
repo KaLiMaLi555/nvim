b52a8d73bfb0cc88e339959489b31691c32bb90a
kirtan-mali <kirtan.mali@galleri5.com>
2025-05-21 00:08:21 +0530

    feat: Undotree

---
+++ lua/plugins/undotree.lua
@@ -0,0 +1,12 @@
+return {
+	"jiaoshijie/undotree",
+	dependencies = "nvim-lua/plenary.nvim",
+	config = true,
+	keys = {
+		{
+			"<localleader>u",
+			"<cmd>lua require('undotree').toggle()<cr>",
+			desc = "Toggle Undo Tree",
+		},
+	},
+}
