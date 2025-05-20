03dbc07e28bdabb6ca70724a40dbd571be3445fd
kirtan-mali <kirtan.mali@galleri5.com>
2025-05-21 00:16:48 +0530

    Some extra QoL plugins

---
+++ lua/plugins/extras.lua
@@ -0,0 +1,21 @@
+return {
+	{
+        "tpope/vim-repeat",
+        event = { "BufReadPre", "BufNewFile" },
+    },
+    {
+        "numToStr/Comment.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        config = true,
+    },
+    {
+        "kylechui/nvim-surround",
+        event = { "BufReadPre", "BufNewFile" },
+        config = true,
+    },
+    {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {},
+    },
+}
