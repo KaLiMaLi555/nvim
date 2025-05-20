8621a423dfb18884a9656e70be868627c8cfe27c
kirtan-mali <kirtan.mali@galleri5.com>
2025-05-21 00:06:05 +0530

    Trouble for better diagnostic view

--- lua/plugins/snacks/init.lua
+++ lua/plugins/snacks/init.lua
@@ -3,6 +3,7 @@ return {
 		"folke/snacks.nvim",
 		priority = 1000,
 		lazy = false,
+		---@diagnostic disable-next-line: undefined-doc-name
 		---@type snacks.Config
 		opts = {
 			dashboard = require("plugins.snacks.dashboard"),
